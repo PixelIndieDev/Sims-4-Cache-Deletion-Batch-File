cad740717845cce9b13681b0586de456049e275e
V <89741457+SenpaiKisser@users.noreply.github.com>
2023-03-16 19:28:06 +0100

    Add files via upload

---
+++ DeleteSimsCache.bat
@@ -0,0 +1,4 @@
+DEL /q /F "%UserProfile%\Documents\Electronic Arts\The Sims 4\localthumbcache.package"
+DEL /q /F "%UserProfile%\Documents\Electronic Arts\The Sims 4\cachestr\*.*"
+DEL /q /F "%UserProfile%\Documents\Electronic Arts\The Sims 4\cache\*.*"
+DEL /q /F "%UserProfile%\Documents\Electronic Arts\The Sims 4\onlinethumbnailcache\*.*"
