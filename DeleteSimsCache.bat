714ceaf827dc3a88f15fbe9d332e379c2f73d30b
V <89741457+PixelIndieDev@users.noreply.github.com>
2024-09-12 19:47:27 +0200

    Updated DeleteSimsCache.bat
    Rewritten some code

--- DeleteSimsCache.bat
+++ DeleteSimsCache.bat
@@ -1,46 +1,50 @@
 @echo off
 
-::Change folderpath in case the script can't find the folder
+:: Change folderpath in case the script can't find the folder
 set folderpath="%UserProfile%\Documents"
-::Change showMessages to false if you don't want to see the message pop-up
+:: Change showMessages to true if you want to see the message pop-up
 set showMessages=true
+:: Variable used to check if at least one file got deleted. Do NOT manually change value
+set somethingDeleted=false
 
-::Check if the folder exists
-if exist "%folderpath%\Electronic Arts\The Sims 4\cachestr\" (
-	DEL /q /F "%folderpath%\Electronic Arts\The Sims 4\localthumbcache.package"
-	DEL /q /F "%folderpath%\Electronic Arts\The Sims 4\cachestr\*.*"
-	DEL /q /F "%folderpath%\Electronic Arts\The Sims 4\cache\*.*"
-	DEL /q /F "%folderpath%\Electronic Arts\The Sims 4\onlinethumbnailcache\*.*"
-	set message=Sims 4 Cache has been deleted successfully
-		if [%showMessages%]==[true] (
-			goto sendMessage
-		) else (
-			exit
-		)
-)
+goto :main
 
-::Check if folder is located in the onedrive of the user
-if exist "C:\Users\%UserProfile%\OneDrive\Documents\Electronic Arts\The Sims 4\cachestr\" (
-	DEL /q /F "C:\Users\%UserProfile%\OneDrive\Documents\Electronic Arts\The Sims 4\localthumbcache.package"
-	DEL /q /F "C:\Users\%UserProfile%\OneDrive\Documents\Electronic Arts\The Sims 4\cachestr\*.*"
-	DEL /q /F "C:\Users\%UserProfile%\OneDrive\Documents\Electronic Arts\The Sims 4\cache\*.*"
-	DEL /q /F "C:\Users\%UserProfile%\OneDrive\Documents\Electronic Arts\The Sims 4\onlinethumbnailcache\*.*"
-	set message=Sims 4 Cache has been deleted successfully
-		if [%showMessages%]==[true] (
-			goto sendMessage
-		) else (
-			exit
-		)
+:: Function to delete files and check if they exist
+:deleteFiles
+if exist %1 (
+	DEL /q /F %1
+	set somethingDeleted=true
 ) else (
-	set message=Sims 4 Cache folder has not been found
-		if [%showMessages%]==[true] (
-			goto sendMessage
-		) else (
-			exit
-		)
+	echo [31mFile or folder not found: %1[0m
+)
+EXIT /B
+
+:: Check if the folder exists and delete files from custom folder path
+:main
+if exist "%folderpath%\Electronic Arts\The Sims 4" (
+	call :deleteFiles "%folderpath%\Electronic Arts\The Sims 4\localthumbcache.package"
+	call :deleteFiles "%folderpath%\Electronic Arts\The Sims 4\cachestr\*.*"
+	call :deleteFiles "%folderpath%\Electronic Arts\The Sims 4\cache\*.*"
+	call :deleteFiles "%folderpath%\Electronic Arts\The Sims 4\onlinethumbnailcache\*.*"
+) else (
+	echo [41;30mSims 4 Documents folder path not found: %folderpath%\Electronic Arts\The Sims 4[0m
 )
 
-::goto's
+:: Report results based on whether files were deleted
+if [%somethingDeleted%]==[true] (
+	set message=[4;92m'Sims 4 Cache Deletion Batch File' File has ran successfully[0m
+) else (
+	set message=[41;30mSomething went wrong with running the 'Sims 4 Cache Deletion Batch File'[0m
+)
+
+:: Display message if showMessages is true
+if [%showMessages%]==[true] (
+	goto sendMessage
+) else (
+	exit
+)
+
+:: goto's
 :sendMessage
 echo.
 echo %message%
